87e0454faa970ffa7f3cb02c9ea6c1d088db384a
MannatGoswami <mannatgoswami08@gmail.com>
2026-07-05 15:04:47 +0530

    add variables

---
+++ C_Basics/02_VARIABLES.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int age =10;
+    float height=4.5;
+    char name ="arjun";
+
+
+    printf("age =%d\n", age);
+    printf("height =%f\n", height);
+    printf("name=%c\n",name);
+
+    return 0;
+}
+
