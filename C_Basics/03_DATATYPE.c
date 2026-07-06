eefa6d63338ca24cc82241e10716fbe14386737d
MannatGoswami <mannatgoswami08@gmail.com>
2026-07-06 13:17:14 +0530

    add datatype

---
+++ C_Basics/03_DATATYPE.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+    int number = 100;
+    float decimal= 12.33;
+    double pi=3.145678393;
+    char letter = "MANNAT";
+    
+
+    printf("number %d\n",number);
+    printf("decimal %2f\n",decimal);
+    printf("pi %10f\n",pi);
+    printf ("letter %c\n",letter);
+
+
+    return 0;
+}
