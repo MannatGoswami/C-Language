2f6e02811595dfc288bfc4d5f26144f281795cbd
MannatGoswami <mannatgoswami08@gmail.com>
2026-07-07 12:09:48 +0530

    add area of circle

---
+++ C_Basics/12_AREA_OF_CIRCLE.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float radius, area;
+
+    printf("Enter radius: ");
+    scanf("%f", &radius);
+
+    area = 3.14 * radius * radius;
+
+    printf("Area = %f", area);
+
+    return 0;
+}
